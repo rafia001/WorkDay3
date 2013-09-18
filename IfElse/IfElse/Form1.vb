3988e7887981149b91b62b4705a8220b569e87e0
idsc3101 <idsc3101@gmail.com>
2013-09-18 17:25:51 -0500

    I solved it

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -3,6 +3,15 @@
 Public Class Form1
 
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
+        Dim a, b, largerNum As Double
+        a = CDbl(txtFirstNum.Text)
+        b = CDbl(txtSecondNum.Text)
+        If (a > b) Then
+            largerNum = a
+        Else
+            largerNum = b
+        End If
+        txtResult.Text = " The larger number is " & largerNum
 
     End Sub
 End Class
